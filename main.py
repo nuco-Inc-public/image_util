7ca4a8b92deb7520438e9054affc7e393dfcea8a
Shogo-Sekine <gan.gun.kyosho@gmail.com>
2018-05-17 19:06:46 +0900

    作業

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
 # coding:utf-8
+import os
+from module import add_result_on_image
+from module import grad_cam
 
-def main(args):
+def init():
+    print("initializing...")
+    os.system("rm -r ./output/")
+
+
+def create_thumbnail(image_path):
     pass
+
+def test():
+    print('やったね！')
+    exit()
+
+def make_directory(directory_path):
+    if os.path.exists(directory_path):
+        print('directory already exists')
+        return
+    os.system('mkdir ./output/' + directory_path)
+
+
+def remove_directory(directory_path):
+    if not os.path.exists(directory_path):
+        print('directory does not exist')
+        return
+    os.system('remove ./output/' + directory_path)
+
+
+def add_result_on_image(image_path, result):
+    if not os.path.exists(image_path):
+        print('image not found.')
+        exit()
+
+    add_result_on_image.add(image_path, result)
+    
+
+def grad_cam(model, image_path, layer_name, file_name=image_path.split('/')[-1]):
+    img = image.load_img(image_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    cam = grad_cam.run(model, x[0], layer_name)
+    img = array_to_img(cam)
+    img.save('./output/tmp/grad_cam/' + str(file_name) + '.jpg')
--- module/add_result_on_image.py
+++ module/add_result_on_image.py
@@ -2,12 +2,10 @@
 
 
 
-def write_result_per_image(image_path, result):
-    file_name = image_path.split('/')[-1]
+def add(image_path, result, file_name = image_path.split('/')[-1]):
     text = 'OK' if acc else 'NG'
     fill = ('#0000ff') if acc else ('#ff0000')
     image = Image.open(image_path).resize((224, 224))
     draw = ImageDraw.Draw(image)
     draw.text((draw_x, draw_y), text, font=font, fill=fill)
-    # TODO インデックスでリネーム
-    image.save("../output/images/" + type_ + "/" + file_name)
+    image.save("./output/tmp/" + type_ + "/" + file_name)
