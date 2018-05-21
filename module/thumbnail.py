b910e83b2ac78a6f77fe7cd16806e0d7dcc13fb1
Shogo-Sekine <gan.gun.kyosho@gmail.com>
2018-05-21 18:45:15 +0900

    fixed log output

--- module/thumbnail.py
+++ module/thumbnail.py
@@ -75,6 +75,6 @@ def create_thumbnail(type_, file_name):
             row_list = []
             continue
     create_concat_tile(img_list2D).save("./util/image_util/output/thumbnail/" + type_ + "/" + type_+file_name + ".jpg")
-    print('Craeted thumnail type: %s, file_name: %s' %(type_, file_name))
+    print('Craeted thumnail type: %s, file_name: %s' %(type_, type_+file_name))
 
     
