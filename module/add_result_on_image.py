a1dc014bb3bafeb6ca68ce2103ed92364e705cf0
ishiguro <yu_ishiguro@nuco.co.jp>
2018-05-22 13:40:30 +0900

    RGB変換処理を追加

--- module/add_result_on_image.py
+++ module/add_result_on_image.py
@@ -7,7 +7,6 @@ from PIL import ImageFont
 def add(image_path, result, file_name=None):
     if file_name == None:
         file_name = image_path.split('/')[-1]
-    
     font_size = 40
     draw_x = 30
     draw_y = 30
@@ -15,6 +14,7 @@ def add(image_path, result, file_name=None):
     text = 'OK' if result else 'NG'
     fill = '#0000ff' if result else '#ff0000'
     image = Image.open(image_path).resize((224, 224))
+    image = image.convert('RGB')
     draw = ImageDraw.Draw(image)
     draw.text((draw_x, draw_y), text, font=font, fill=fill)
     image.save("./util/image_util/output/tmp/" + file_name + ".jpg")
